191f1fd0845c18e3ceca347d949b0483bdf5cccd
ekaterina <whitedevil93@mail.ru>
2023-01-29 18:51:56 +0300

    exp

---
+++ дз3.3.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число степени: ");
+int B = int.Parse(Console.ReadLine());
+
+int Sum = 1;
+for (int i = 1; i <= B; i++)
+{
+    Sum*=A;
+}
+Console.WriteLine(Sum);
