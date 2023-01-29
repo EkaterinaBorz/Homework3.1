8c5bf00d052ca73c0517dc1f86fa05d411507742
ekaterina <whitedevil93@mail.ru>
2023-01-29 17:38:15 +0300

    куб

---
+++ дз3.1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine();
+
+Console.Write("Введите число:   ");
+string write = Console.ReadLine();
+int N = Convert.ToInt32(write);
+
+
+int[] GetCubeTable(int N)
+{
+    int[] cubs = new int[N];
+    int number = 1;
+    for (int i = 0; i < N; i++)
+    {
+        cubs[i] = number * number * number;
+        number++;
+    }
+    return cubs;
+}
+
+void PrintCubs(int[] array)
+{
+    int number = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{number} ^ 3 = ");
+        Console.WriteLine(array[i]);
+        number++;
+    }
+}
+
+int[] CubeTable = GetCubeTable(N);
+PrintCubs(CubeTable);
