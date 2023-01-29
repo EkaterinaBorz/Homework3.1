656c85923b42b642316bb07dfcf24d4a92c5bb79
ekaterina <whitedevil93@mail.ru>
2023-01-29 19:57:36 +0300

    cube

---
+++ дз3.6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+int number = 1;
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+while (number <= N)
+{
+    int cube = number * number * number;
+    if (cube % 2 == 0) Console.Write(cube + " ");
+    number++;
+}
+Console.WriteLine();
