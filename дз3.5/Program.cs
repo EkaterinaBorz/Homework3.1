e0eecbbf4dee692628f97b584a458ed8207f7c62
ekaterina <whitedevil93@mail.ru>
2023-01-29 19:23:58 +0300

    prodNumb

---
+++ дз3.5/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine();
+Console.Write("Введите число: ");
+
+string writeN = Console.ReadLine();
+
+int N = Convert.ToInt32(writeN);
+
+int GetNumberProduct(int i)
+{
+  int prod = 1;
+  
+
+  for (int numb = 1; numb <= i; numb++)
+  {
+    prod = prod * numb;
+    
+  }
+
+  return prod;
+}
+
+
+Console.WriteLine($" Произведение чисел от 1 до {N} составляет {GetNumberProduct(N)}");
