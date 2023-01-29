a432c3d4b385e6aeecf2fdcccd1991377843ee37
ekaterina <whitedevil93@mail.ru>
2023-01-29 18:07:39 +0300

    summ

---
+++ дз3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine();
+Console.Write("Введите число: ");
+
+string writeA = Console.ReadLine();
+
+int A = Convert.ToInt32(writeA);
+
+int GetNumberSum(int A)
+{
+  int number = 1;
+  int sum = 0;
+
+  for (int i = 0; i < A; i++)
+  {
+    sum = sum + number;
+    number++;
+  }
+
+  return sum;
+}
+
+int NumberSum = GetNumberSum(A);
+Console.WriteLine($" Сумма чисел от 1 до{A} составляет {NumberSum}");
