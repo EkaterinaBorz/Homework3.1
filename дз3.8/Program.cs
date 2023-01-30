26ad6fad5db5697f6b53c6f3c06174e7c717ccf7
ekaterina <whitedevil93@mail.ru>
2023-01-30 20:43:46 +0300

    massMiss

---
+++ дз3.8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+
+int[] array = new int[5];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = new Random().Next(0, 20);
+  Console.Write(array[i] + " ");
+
+}
+Console.WriteLine();
+
+Console.Write("Введи число, которое необходимо найти: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+for (int j = 0; j < array.Length; j++) 
+{
+  if (array[j] == number) count = 1;
+}
+if (count > 0) Console.WriteLine("Такое число в массиве есть. ");
+else Console.WriteLine("Такого числа в массиве нет.");
+
