c555c898da596c35d3372b414dbd4fc5cb4e13a1
ekaterina <whitedevil93@mail.ru>
2023-02-02 23:01:45 +0300

    countNotcount

---
+++ дз3.9/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int[] array = new int[20];
+int count = 0;
+int Notcount = 0;
+
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = new Random().Next(100, 1000);
+  Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for (int j = 0; j < array.Length; j++)
+{
+if (array [j]%2 == 0 ) count = count +1;
+
+else Notcount = Notcount + 1;
+
+}
+Console.WriteLine(" Четные числа из массива: " + count);
+Console.WriteLine();
+Console.WriteLine(" Нечетные числа из массива: " + Notcount);
