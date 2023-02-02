637f3663eae0409b7b69647fb319c9918b9552ea
ekaterina <whitedevil93@mail.ru>
2023-02-03 02:00:06 +0300

    masArr

---
+++ дз3.12/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+Console.Clear();
+int[] array = new int[11];
+Console.WriteLine();
+for(int i = 0; i < array.Length; i++)
+{
+  array[i] = new Random().Next(0, 10);
+  Console.Write( array[i] + " ");
+}
+Console.WriteLine();
+
+int j = array.Length - 1;
+for ( int i = 0; i< (array.Length +1)/2; i++)
+{
+  Console.Write( array[i] * array [j] + " ");
+  j--;
+}
