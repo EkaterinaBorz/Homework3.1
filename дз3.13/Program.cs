b6ab720e3bb9c536bf874b58e39a518a82a89bad
ekaterina <whitedevil93@mail.ru>
2023-02-03 03:13:32 +0300

    minMax

---
+++ дз3.13/Program.cs
@@ -0,0 +1,23 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+
+Console.Clear();
+int[] array = new int [20];
+int count = 0;
+for ( int i = 0; i < array.Length; i++)
+{
+  array[i] = new Random().Next(1, 1000);
+  Console.Write( array[i] + " ");
+}
+
+int max = array[0];
+int min = array[0];
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] > max) max = array[j];
+    if (array[j] < min) min = array[j];
+}
+Console.WriteLine();
+Console.Write("Разница между максимальным и минимальным элементом = ");
+Console.Write(max - min);
